363c92c46ff2e9d19f906a383768f28a414713e2
andreluas <andreluas.k@gmail.com>
2021-04-14 18:52:50 -0300

    add const generateTemplate (todo) :heavy_check_mark:

--- src/script/script.js
+++ src/script/script.js
@@ -3,3 +3,18 @@ const addButton = document.querySelector('.add-todo');
 const todoList = document.querySelector('.todos');
 const list = document.querySelector('.todos li');
 
+let listLenght = list.lenght;
+
+const generateTempalate = (todo) => {
+    const html = `
+        <li>
+            <input type="checkbox" id="todo_${listLenght}">
+            <label for="todo_${listLenght}">
+                <span class="check"></span>
+                ${todo}
+            </label>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `
+};
+
