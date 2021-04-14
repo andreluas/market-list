252ed39411a3362d6e1f243b9fb9da70869a6434
andreluas <andreluas.k@gmail.com>
2021-04-14 18:41:17 -0300

    add const

---
+++ src/script/script.js
@@ -0,0 +1,5 @@
+const subimitForm = document.querySelector('.add');
+const addButton = document.querySelector('.add-todo');
+const todoList = document.querySelector('.todos');
+const list = document.querySelector('.todos li');
+
