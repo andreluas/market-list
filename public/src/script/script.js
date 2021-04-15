ba6fdb9f1876af839593c59d1b60983adb3288d3
andreluas <andreluas.k@gmail.com>
2021-04-14 23:45:22 -0300

    sw/manifest up :zap:

---
+++ public/src/script/script.js
@@ -0,0 +1,43 @@
+const submitForm = document.querySelector('.add');
+const addButton = document.querySelector('.add-todo');
+const todoList = document.querySelector('.todos');
+const list = document.querySelectorAll('.todos li');
+
+let listLenght = list.lenght;
+
+// GENERATE TODO
+const generateTodo = (todo) => {
+    const html = `<li>
+                    <input type="checkbox" id="todo_${listLenght}">
+                    <label for="todo_${listLenght}">
+                    <span class="check"></span>
+                    ${todo}
+                    </label>
+                    <i class="far fa-trash-alt delete"></i>
+                </li>`
+    todoList.innerHTML += html;
+};
+
+// ADD TODOS FUNCTION
+function addTodos(e) {
+    e.preventDefault();
+    const todo = submitForm.add.value.trim();
+
+    if(todo.length) {
+        listLenght = listLenght + 1;
+        generateTodo(todo);
+        submitForm.reset();
+    }
+}
+
+submitForm.addEventListener('submit', addTodos);
+addButton.addEventListener('click', addTodos);
+
+// REMOVE TODOS FUNCTION
+function deleteTodos(e) {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+}
+
+todoList.addEventListener('click', deleteTodos);
