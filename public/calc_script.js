3749a6ab7888971a9f5fe9b67af5a539b06b2fa1
andreluas <andreluas.k@gmail.com>
2021-04-16 22:34:30 -0300

    number keypress event :heavy_check_mark:

--- public/calc_script.js
+++ public/calc_script.js
@@ -101,3 +101,29 @@ clearLastEl.addEventListener('click', (e) => {
     dis2Num = '';
 });
 
+// number keypress event
+window.addEventListener('keydown', (e) => {
+    if(
+        e.key === '0' || 
+        e.key === '1' || 
+        e.key === '2' || 
+        e.key === '3' || 
+        e.key === '4' || 
+        e.key === '5' || 
+        e.key === '6' || 
+        e.key === '7' || 
+        e.key === '8' || 
+        e.key === '9' || 
+        e.key === '.'  
+    ){
+        clickButtonEl(e.key);
+    }
+});
+
+function clickButtonEl(key) {
+    numbersEl.forEach(button => {
+        if(button.innerText === key) {
+            button.click();
+        }
+    })
+}
